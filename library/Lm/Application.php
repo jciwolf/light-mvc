9d86e633a50eae327488d4ccdaf535fc54a8a6ef
wangbai <wangbai@yongche.com>
2013-09-30 14:53:09 +0800

    finish Response

--- library/Lm/Application.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class Lm_Application {
-
-}
--- library/Lm/Component/Response.php
+++ library/Lm/Component/Response.php
@@ -4,12 +4,48 @@ class Lm_Component_Response {
 
     private $body = null;
 
-    private 
     private $headers = null;
 
     private $httpCode = 200;
 
     public function setBody($body) {
-        $this->body = 
+        $this->body = $body;
+        return;
+    }
+
+    public function getBody() {
+        return $this->body;
+    }
+
+    public function setHttpCode($httpCode) {
+        $this->httpCode = $httpCode;
+        return;
+    }
+
+    public function getHttpCode() {
+        return $this->httpCode;
+    }
+
+    public function setHeader($key, $value) {
+        $this->_headers[] =  array ( $key => $value );
+        return;
+    }
+
+    public function getHeaders() {
+        return $this->_headers;
+    }
+
+    public function output() {
+        //output header
+        foreach ($this->getHeaders as $header) {
+            header($header['name'] . ': ' . $header['value']);
+        }
+
+        //output http code
+        http_response_code($this->getHttpCode());
+
+
+        //output body
+        echo $this->getBody();
     }
 }
