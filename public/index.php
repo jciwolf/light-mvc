3a9fbc3d943390b0e620741bd4e475501e27f755
wangbai <wangbai@yongche.com>
2013-10-08 18:44:25 +0800

    delete xhprof

--- public/index.php
+++ public/index.php
@@ -3,16 +3,20 @@
 //load bootstrap
 require ("../application/bootstrap.php");
 
+/*
 //profile
 require("/usr/share/xhprof/xhprof_lib/utils/xhprof_lib.php");
 require("/usr/share/xhprof/xhprof_lib/utils/xhprof_runs.php");
 
 xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
+*/
 
 //start application
 $application = new Lm_Application_Base(realpath(dirname(__FILE__))."/../");
 $application->run();
 
+/*
 $xhprof_data = xhprof_disable();
 $xhprof_runs = new XHProfRuns_Default();
 $run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_lightmvc");
+*/
